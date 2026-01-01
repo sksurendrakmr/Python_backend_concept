968fa579caad63d3f6ddad616fe29308acb7b226
sksurendrakmr <sksurendrakmr@gmail.com>
2026-01-01 23:09:39 +0530

    Python type hinting with type alias

---
+++ TypeHinting.py
@@ -0,0 +1,36 @@
+"""
+Type hint let us add type information to python code, which can help us write self-documenting code,
+catch bugs earlier and get better IDE completions.
+
+In python, type hinting doesn't give error on type mismatch. We need type checker for that.
+ - mypy -> uvx mypy <file_name>
+
+ Instead of age: int | None -> age: Optional[int] (older way)
+"""
+
+from typing import NewType, Tuple
+
+RGB = NewType("RGB", Tuple[int, int, int])
+HSL = NewType("HSL", Tuple[int, int, int])
+
+# type alias -> older version
+UserReturnType = dict[str, str | int | RGB | None]
+
+# type alias with python new version
+type UserReturnTypeNew = dict[str, str | int | None]
+
+
+def create_user(first_name: str, last_name: str, age: int | None = None,
+                fav_color: RGB | None = None) -> UserReturnType:
+    email = f"{first_name.lower()}_{last_name.lower()}@example.com"
+    return {
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": email,
+        "age": age,
+        "fav_color": fav_color,
+    }
+
+
+user1 = create_user("John", "Doe")
+user2 = create_user("Oliver", "Doe", age=22, fav_color=RGB((200,90,78)))
