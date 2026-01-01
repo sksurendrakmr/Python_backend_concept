43099d8ac1998685b7f1bec3bad59f9cca92c625
sksurendrakmr <sksurendrakmr@gmail.com>
2026-01-01 18:09:49 +0530

    SQLAlchemy notes

--- sqlalchemy_basics.py
+++ sqlalchemy_basics.py
@@ -6,6 +6,11 @@ Sqlalchemy has two packages:
     SQLAlchemy Core deal with connection.
     SQLAlchemy ORM deal with session.
 
+    This way of using SQLAlchemy is to run raw SQL directly. But this is now SQLAlchemy is used for.
+
+    With SQLAlchemy, either we used its core package where we can use functions and metadata to create everything
+    in the database easily, or we use an ORM(Object Relational Mapper) to map python classes to database tables.
+
 
 '''
 from sqlalchemy import create_engine, text
@@ -16,3 +21,9 @@ db_connection = engine.connect()
 db_connection.execute(text("CREATE TABLE IF NOT EXISTS people (name str, age int)"))
 db_connection.commit()
 
+from sqlalchemy.orm import Session
+
+session = Session(engine)
+
+session.execute(text("INSERT INTO people (name, age) VALUES ('john', 27);"))
+session.commit()
