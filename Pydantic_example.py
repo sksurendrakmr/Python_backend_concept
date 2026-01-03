8a2f8ade927b08595ea5e06563aa21f0e950b8d7
sksurendrakmr <sksurendrakmr@gmail.com>
2026-01-03 17:46:11 +0530

    pydantic model access and modification

--- Pydantic_example.py
+++ Pydantic_example.py
@@ -1,6 +1,11 @@
 from pydantic import BaseModel, ValidationError
 from datetime import datetime
 
+'''
+    Model instances are mutable by default and also by default they don't revalidate when we change a field.
+    We can change this default behaviour in model configuration.
+'''
+
 
 # Pydantic will use this property to validate the data at runtime.
 class User(BaseModel):
@@ -14,5 +19,23 @@ class User(BaseModel):
     verified_at: datetime | None = None
 
 
-user = User(uid = 23,username="sk", email="email")
+user = User(uid=23, username="sk", email="email")
 print(user)
+
+# Modifying and accessing data
+print(user.username)  # access
+
+# setting of value
+user.bio = "123"
+print(user.bio)
+
+# to convert pydantic model to dictionary
+print(user.model_dump())
+
+# to get JSON
+print(user.model_dump_json(indent=2))  # indent format the JSON with 2 spaces.
+
+'''
+    model_dump() and model_dump_json() are useful when we need to serialize our model for storage or sending over a network.
+    Serialize means converting the Python object into a simple format that can be easily saved to a file or sent to different systems.
+'''
